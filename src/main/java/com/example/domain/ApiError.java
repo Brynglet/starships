d7870fb2b0844e43add70fcfa34a0c65ea5646b0
brynglet <jbryngel@gmail.com>
2023-04-09 21:48:02 +0200

    Cleanup

--- src/main/java/com/example/domain/ApiError.java
+++ src/main/java/com/example/domain/ApiError.java
@@ -1,16 +1,12 @@
 package com.example.domain;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatusCode;
 
 import java.time.ZonedDateTime;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class ApiError extends RuntimeException {
 
     private HttpStatusCode httpStatusCode;
