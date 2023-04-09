cd4cba8c8a52771c8bc44e42e6a928ae562be8a7
brynglet <jbryngel@gmail.com>
2023-04-09 19:58:06 +0200

    Config

--- src/main/java/com/example/config/WebClientConfig.java
+++ src/main/java/com/example/config/WebClientConfig.java
@@ -1,8 +1,10 @@
 package com.example.config;
 
 import com.example.facade.Endpoint;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Component
@@ -10,9 +12,9 @@ public class WebClientConfig {
 
     @Bean
     @Endpoint(Endpoint.APISystem.SWAPI)
-    public WebClient swapiEndpoint() {
+    public WebClient swapiEndpoint(@Value("${swapi.ingress.uri}") String swapiUri) {
         return WebClient.builder()
-                .baseUrl("https://swapi.dev/api/")
+                .baseUrl(swapiUri)
                 .build();
     }
 }
